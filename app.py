361fc3fb2d5500d263a99a8c2747e995e809294b
GolaganaGanesh <119392026+GolaganaGanesh@users.noreply.github.com>
2024-01-25 02:03:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, Flask Web App!'
+def hello_world():
+    return 'Hello, this is my Flask web app!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
