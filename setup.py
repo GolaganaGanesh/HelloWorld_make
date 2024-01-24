658a448672984955d9021f9b08adf9db61a6c01c
GolaganaGanesh <119392026+GolaganaGanesh@users.noreply.github.com>
2024-01-25 02:01:19 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='your-web-app',
+    version='1.0.0',
+    packages=['your_web_app'],
+    install_requires=[
+        'Flask',
+    ],
+)
